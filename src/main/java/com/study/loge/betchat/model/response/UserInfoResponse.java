29be625b70f2156551836e3e8c96cebc06777d0a
yw9594 <yw9594@naver.com>
2021-02-09 01:01:17 +0900

    dev-be-comment-changed

--- src/main/java/com/study/loge/betchat/model/response/UserInfoResponse.java
+++ src/main/java/com/study/loge/betchat/model/response/UserInfoResponse.java
@@ -5,7 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-// 클라이언트에게 유저 아이디를 전달하기 위한 클래스입니다.
+// 클라이언트에게 유저 키를 전달하기 위한 클래스입니다.
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
