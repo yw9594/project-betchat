7a683fe37b15c5eeba62b8e794b442249ec08f9d
yw9594 <yw9594@naver.com>
2021-02-09 01:02:17 +0900

    dev-be-room-create-req-res-added

---
+++ src/main/java/com/study/loge/betchat/model/requests/RoomCreateRequest.java
@@ -0,0 +1,15 @@
+package com.study.loge.betchat.model.requests;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+// 채팅방을 생성하길 바라는 클라이언트로부터 user key를 전달받는 클래스입니다.
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class RoomCreateRequest {
+    private String userKey;  // 채팅방 생성 요청 유저 Key
+}
