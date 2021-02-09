b641ad72c84b064aba120b836bc2aeb2660ebcd6
yw9594 <yw9594@naver.com>
2021-02-09 20:57:25 +0900

    dev-be-simple-chat-controller-added

--- src/main/java/com/study/loge/betchat/config/WebSocketConfig.java
+++ src/main/java/com/study/loge/betchat/config/WebSocketConfig.java
@@ -11,8 +11,8 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        // Web Socket handshake를 위한 주소입닏.
-        registry.addEndpoint("/ws");
+        // Web Socket handshake를 위한 주소입니다.
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
     }
 
     @Override
---
+++ src/main/java/com/study/loge/betchat/controller/ChatController.java
@@ -0,0 +1,11 @@
+package com.study.loge.betchat.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// 채팅방의 메시지를 처리하는 클래스입니다.
+@Controller
+@RequestMapping("/chat")
+public class ChatController {
+}
