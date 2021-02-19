769e1ef657182c58cc3a4585c4b5c006a5d6224a
yw9594 <yw9594@naver.com>
2021-02-20 02:09:33 +0900

    dev-be-room-entity-class-added

---
+++ src/main/java/com/study/loge/betchat/entity/Room.java
@@ -0,0 +1,35 @@
+package com.study.loge.betchat.entity;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+// room entity를 표현하기 위한 entity 클래스입니다.
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String roomKey;
+    private Integer activated;
+    private LocalDateTime localDateTime;
+
+    @Override
+    public String toString() {
+        return "Room{" +
+                "id=" + id +
+                ", roomKey='" + roomKey + '\'' +
+                ", activated=" + activated +
+                ", localDateTime=" + localDateTime +
+                '}';
+    }
+}
