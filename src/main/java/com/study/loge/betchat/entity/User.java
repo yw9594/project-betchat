395c686391d3effb55b91060a01dff68b0331a2f
yw9594 <yw9594@naver.com>
2021-02-20 00:57:44 +0900

    dev-be-user-entity-create-added

--- src/main/java/com/study/loge/betchat/entity/User.java
+++ src/main/java/com/study/loge/betchat/entity/User.java
@@ -1,30 +1,29 @@
 package com.study.loge.betchat.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
-// User Entity를 표현하기 위한 클래스입니다.
+// user entity를 표현하기 위한 entity 클래스입니다.
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
+@Builder
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String userName;
     private String userKey;
-    private String activated;
-    private LocalDate createdAt;
+    private Integer activated;
+    private LocalDateTime createdAt;
 
     @Override
     public String toString() {
--- src/main/java/com/study/loge/betchat/service/UserLoginService.java
+++ src/main/java/com/study/loge/betchat/service/UserLoginService.java
@@ -1,14 +1,19 @@
 package com.study.loge.betchat.service;
 
 import com.study.loge.betchat.component.KeyGenerator;
+import com.study.loge.betchat.entity.User;
 import com.study.loge.betchat.enums.ResultState;
 import com.study.loge.betchat.enums.UserLoginValidationState;
 import com.study.loge.betchat.exceptions.UserLoginException;
 import com.study.loge.betchat.model.MessageHeader;
 import com.study.loge.betchat.model.requests.UserLoginRequest;
 import com.study.loge.betchat.model.response.UserLoginResponse;
+import com.study.loge.betchat.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 // user login 로직을 처리합니다.
 @AllArgsConstructor
@@ -17,23 +22,41 @@ public class UserLoginService {
     // userKey를 생성하기 위한 객체입니다.
     private final KeyGenerator keyGenerator;
 
+    // DB에 접근하기 위한 repository 객체입니다.
+    private UserRepository userRepository;
+
     // 유저의 로그인 요청을 처리합니다.
     public MessageHeader<UserLoginResponse> userLogin(MessageHeader<UserLoginRequest> request){
-        ResultState resultState=null;
+        ResultState resultState = null;
+        String userKey = null;
 
         String userName = request.getData().getUserName();
         try {
             // userName의 유효성을 검사합니다.
             checkUserNameValidation(userName);
 
+            // response에 필요한 값을 생성합니다.
             resultState = ResultState.OK;
+            userKey = keyGenerator.generateKey();
+
+            // DB에 저장합니다.
+            User user = User.builder()
+                    .userName(userName)
+                    .userKey(userKey)
+                    .activated(1)
+                    .createdAt(LocalDateTime.now())
+                    .build();
+            userRepository.save(user);
         }
         catch(UserLoginException e){
             resultState = ResultState.ERROR;
         }
         finally {
-            // response를 생성해 전달합니다.
-            return MessageHeader.makeMessage(resultState, createUserLoginResponse(resultState));
+            // response를 생성한 뒤, 반환한다.
+            UserLoginResponse userLoginResponse = UserLoginResponse.builder()
+                    .userKey(userKey)
+                    .build();
+            return MessageHeader.makeMessage(resultState, userLoginResponse);
         }
     }
 
@@ -43,14 +66,5 @@ public class UserLoginService {
 
         if(!userName.matches(pattern))
             throw new UserLoginException();
-    }
-
-    // resultState에 따른 UserLoginResponse 객체를 생성합니다.
-    private UserLoginResponse createUserLoginResponse(ResultState resultState){
-        String userKey = resultState==ResultState.OK ? keyGenerator.generateKey() : null;
-
-        return UserLoginResponse.builder()
-                .userKey(userKey)
-                .build();
     }
 }
