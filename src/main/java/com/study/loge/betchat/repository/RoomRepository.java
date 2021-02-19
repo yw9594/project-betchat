fec156bfeeb2b1b24a90d02edb59333468876f07
yw9594 <yw9594@naver.com>
2021-02-20 02:09:47 +0900

    dev-be-room-repository-added

---
+++ src/main/java/com/study/loge/betchat/repository/RoomRepository.java
@@ -0,0 +1,8 @@
+package com.study.loge.betchat.repository;
+
+import org.springframework.stereotype.Repository;
+
+// room table에 접근하기 위한 repository 클래스입니다.
+@Repository
+public interface RoomRepository {
+}
