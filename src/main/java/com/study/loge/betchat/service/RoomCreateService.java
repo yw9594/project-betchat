d13b0e74a31b76d383285777ac00527aee95a92e
yw9594 <yw9594@naver.com>
2021-02-20 02:17:02 +0900

    dev-be-room-create-service-skeleton-added

--- src/main/java/com/study/loge/betchat/service/RoomCreateService.java
+++ src/main/java/com/study/loge/betchat/service/RoomCreateService.java
@@ -1,28 +1,32 @@
-package com.study.loge.betchat.controller;
+package com.study.loge.betchat.service;
 
 import com.study.loge.betchat.component.KeyGenerator;
 import com.study.loge.betchat.enums.ResultState;
 import com.study.loge.betchat.model.MessageHeader;
 import com.study.loge.betchat.model.requests.RoomCreateRequest;
 import com.study.loge.betchat.model.response.RoomCreateResponse;
-import com.study.loge.betchat.service.RoomCreateService;
 import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-// Lobby URL 경로를 제어합니다.
+// room create 로직을 처리합니다.
 @AllArgsConstructor
-@RestController
-@RequestMapping("/lobby")
-public class LobbyController {
-
-    private RoomCreateService roomCreateService;
+@Service
+public class RoomCreateService {
+    // userId를 생성하기 위한 객체입니다.
+    private final KeyGenerator keyGenerator;
 
     // 방 생성 요청을 받고 유저에게 Room Key를 전달합니다.
-    @PostMapping("/create")
     public MessageHeader<RoomCreateResponse> createRoom(@RequestBody MessageHeader<RoomCreateRequest> request){
-        return roomCreateService.createRoom(request);
+        String roomKey = keyGenerator.generateKey();
+        RoomCreateResponse roomCreateResponse = RoomCreateResponse
+                .builder()
+                .roomKey(roomKey)
+                .build();
+        MessageHeader<RoomCreateResponse> response = MessageHeader.makeMessage(ResultState.OK, roomCreateResponse);
+
+        return response;
     }
+
 }
