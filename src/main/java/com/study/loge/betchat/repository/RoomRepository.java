82ba8661f246aa41d3d5ad74eb3b2b7a6a44790d
yw9594 <yw9594@naver.com>
2021-02-20 20:28:23 +0900

    bug-be-room-repository-parent-class-inherited

--- src/main/java/com/study/loge/betchat/repository/RoomRepository.java
+++ src/main/java/com/study/loge/betchat/repository/RoomRepository.java
@@ -1,8 +1,10 @@
 package com.study.loge.betchat.repository;
 
+import com.study.loge.betchat.entity.Room;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 // room table에 접근하기 위한 repository 클래스입니다.
 @Repository
-public interface RoomRepository {
+public interface RoomRepository extends CrudRepository<Room, Long> {
 }
