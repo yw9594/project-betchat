b44828993d761f14af72c212c496c71e57d57ed5
yw9594 <yw9594@naver.com>
2021-04-06 03:30:16 +0900

    Update RoomEntranceMonitor Class comments

--- src/main/java/com/study/loge/betchat/component/RoomEntranceMonitor.java
+++ src/main/java/com/study/loge/betchat/component/RoomEntranceMonitor.java
@@ -9,31 +9,32 @@ import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.stereotype.Component;
 
-// 메세지가 Controller 또는 Broker로 전달하기 전 처리하는 클래스입니다.
+// 채팅방 참가 및 퇴장 시 발생하는 정보를 관리합니다.
 @AllArgsConstructor
 @Component
 public class RoomEntranceMonitor implements ChannelInterceptor {
     private UserParticipateExitService userParticipateExitService;
 
-    // 유저가 채팅방에 참가/퇴장 시 로직을 제어합니다.
     @Override
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
         StompHeaderAccessor stompHeaderAccessor = (StompHeaderAccessor) StompHeaderAccessor.getAccessor(message);
         SimpMessageType messageType = stompHeaderAccessor.getMessageType();
 
+        // Stomp Message Type에 따라 정보를 처리합니다.
         try {
-            // message type에 따라 특정 로직을 수행합니다.
             if(SimpMessageType.SUBSCRIBE.equals(messageType) )
                 userParticipateExitService.processParticipate(message);
             else if(SimpMessageType.DISCONNECT.equals(messageType) ||
                     SimpMessageType.UNSUBSCRIBE.equals(messageType))
                 userParticipateExitService.processExit(message);
 
-        } catch (Exception e) {
-            // 예외가 발생한 경우 subscribe 시키지 않습니다.
+        }
+        // 예외가 발생한 경우 subscribe 시키지 않습니다.
+        catch (Exception e) {
             e.printStackTrace();
             message = null;
-        } finally {
+        }
+        finally {
             return message;
         }
     }
