63b4eeaf2dee49e664009e882120f2a498ad8371
yw9594 <yw9594@naver.com>
2021-06-05 02:34:14 +0900

    Add participate message validation checker class added

---
+++ src/main/java/com/study/loge/betchat/utils/validation/message/ParticipateValidationChecker.java
@@ -0,0 +1,37 @@
+package com.study.loge.betchat.utils.validation.message;
+
+import com.study.loge.betchat.model.entity.Room;
+import com.study.loge.betchat.model.entity.User;
+import com.study.loge.betchat.repository.RoomRepository;
+import com.study.loge.betchat.repository.UserRepository;
+import com.study.loge.betchat.utils.exception.SubscribeException;
+import lombok.AllArgsConstructor;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+
+// 참가 요청에 대한 유효성을 검사합니다.
+@AllArgsConstructor
+@Component
+public class ParticipateValidationChecker extends AbstractMessageValidationChecker{
+    private UserRepository userReposotory;
+    private RoomRepository roomRepository;
+
+    @Override
+    public void isValid(Message<?> message) throws Exception {
+        isKeyValidationCheck(message);
+    }
+
+    private void isKeyValidationCheck(Message<?> message) throws SubscribeException {
+        StompHeaderAccessor stompHeaderAccessor = (StompHeaderAccessor) StompHeaderAccessor.getAccessor(message);
+
+        String userKey = stompHeaderAccessor.getNativeHeader("user_key").get(0);
+        String roomKey = stompHeaderAccessor.getNativeHeader("room_key").get(0);
+
+        User user = userReposotory.findByUserKey(userKey);
+        Room room = roomRepository.findByRoomKey(roomKey);
+
+        // 유효하지 않은 user 또는 room일 경우 예외를 발생시킵니다.
+        if(user==null || room==null) throw new SubscribeException("유효하지 않은 user key 또는 room key입니다.");
+    }
+}
