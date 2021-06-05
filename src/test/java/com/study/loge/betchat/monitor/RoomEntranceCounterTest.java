5150016c551162d6812c9750a65487c2f4997eab
yw9594 <yw9594@naver.com>
2021-06-06 02:26:18 +0900

    Add RoomEntranceCounterTest added & null pointer error occur

---
+++ src/test/java/com/study/loge/betchat/monitor/RoomEntranceCounterTest.java
@@ -0,0 +1,40 @@
+package com.study.loge.betchat.monitor;
+
+import com.study.loge.betchat.utils.parser.StompHeaderParser;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.MessageBuilder;
+
+@SpringBootTest
+public class RoomEntranceCounterTest {
+    @Autowired
+    private RoomEntranceCounter roomEntranceCounter;
+
+    private final String userKey = "userKey";
+    private final String userName = "userName";
+    private final String roomKey = "roomKey";
+    private final String simpSessionId = "testSess";
+
+    private final StompCommand stompCommand = StompCommand.SEND;
+    private Message<?> message;
+
+//    @BeforeEach
+    @Test
+    public void createTestMessge(){
+        StompHeaderAccessor accessor = StompHeaderAccessor.create(stompCommand);
+        accessor.setNativeHeader("user_key", userKey);
+        accessor.setNativeHeader("room_key", roomKey);
+        accessor.setSessionId(simpSessionId);
+
+        MessageHeaders stompHeaders = accessor.getMessageHeaders();
+        message = MessageBuilder.withPayload(new Object()).copyHeaders(stompHeaders).build();
+
+        // null pointer error!
+        System.out.println(StompHeaderParser.getUserKey(message));
+    }
+}
